c0c4e352d565ace3b224e268476460950e6c7961
huhfkkvd <huhfkkvd@atomicmail.io>
2026-09-05 05:08:24 +0330

    Update config.php

--- config.php
+++ config.php
@@ -20,7 +20,7 @@ if ($dbname !== '' && $usernamedb !== '') {
         @mysqli_report(MYSQLI_REPORT_OFF);
     }
     try {
-        $connect = @mysqli_connect('localhost', $usernamedb, $passworddb, $dbname);
+        $connect = @mysqli_connect('mysql.railway.internal', $usernamedb, $passworddb, $dbname);
     } catch (\Throwable $rxMysqliConnectError) {
         $connect = null;
         error_log('config.php mysqli_connect failed: ' . $rxMysqliConnectError->getMessage());
